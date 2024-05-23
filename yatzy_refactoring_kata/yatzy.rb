11753a5d63747482cacc98c53f94e98cb8f84181
adamdawkins <adam@dragondrop.uk>
2024-05-23 12:32:03 +0100

    Use of_a_kind in three_of_a_knid

--- yatzy_refactoring_kata/yatzy.rb
+++ yatzy_refactoring_kata/yatzy.rb
@@ -92,19 +92,8 @@ class Yatzy
       return 0
     end
 
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5)
-      t = [0]*6
-      t[d1-1] += 1
-      t[d2-1] += 1
-      t[d3-1] += 1
-      t[d4-1] += 1
-      t[d5-1] += 1
-      for i in [0,1,2,3,4,5]
-        if (t[i] >= 3)
-          return (i+1) * 3
-        end
-      end
-      0
+    def three_of_a_kind(*args)
+      of_a_kind(*args, 3)
     end
 
     def smallStraight( d1,  d2,  d3,  d4,  d5)
